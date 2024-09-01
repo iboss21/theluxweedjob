f1ed8eaa6ecc119fa106f21a410f0592db25fedb
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-09-01 03:34:22 -0500

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,15 @@
+Locales = {}
+Locales['en'] = {
+    ['pickup_prompt'] = 'Press [E] to pick up weed',
+    ['process_prompt'] = 'Press [E] to process weed',
+    ['sell_prompt'] = 'Press [E] to sell weed',
+    ['no_item'] = 'You don\'t have enough items to do this',
+    ['cooldown_message'] = 'Please wait before performing another action.',
+    ['success_pickup'] = 'You picked up some weed.',
+    ['success_process'] = 'You processed the weed.',
+    ['success_sell'] = 'You sold the weed.',
+    ['fail_pickup'] = 'You cannot carry any more weed.',
+    ['fail_process'] = 'You cannot carry any more processed weed.',
+    ['fail_sell'] = 'You have no processed weed to sell.',
+    ['fail_no_weed'] = 'You have no weed to process.',
+}
