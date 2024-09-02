e80000320dc80b59635f9cdf6b52327eaf12716a
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-09-02 12:10:40 -0500

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,93 +1,91 @@
-local lastActionTime = 0
+local RSGCore = exports['rsg-core']:GetCoreObject()
+local currentLocation = nil
+local cooldowns = {}
 
 -- Helper function to check cooldown
-function isOnCooldown()
-    return GetGameTimer() - lastActionTime < Config.ActionCooldown
+local function isOnCooldown(action)
+    if not cooldowns[action] then return false end
+    return (GetGameTimer() - cooldowns[action]) < Config.ActionCooldown
 end
 
--- Initialize job locations dynamically
-local currentLocation = Config.Locations[math.random(#Config.Locations)]
+-- Helper function to set cooldown
+local function setCooldown(action)
+    cooldowns[action] = GetGameTimer()
+end
 
+-- Initialize job locations
 Citizen.CreateThread(function()
-    if Config.UseTargetSystem then
-        -- Setup target system interaction
-        exports['rsg-target']:AddTargetModel({GetHashKey(Config.PropPickup), GetHashKey(Config.PropProcessing)}, {
+    currentLocation = Config.Locations[math.random(#Config.Locations)]
+    if Config.Debug then
+        print("Current location set to: " .. currentLocation.name)
+    end
+end)
+
+-- Setup target interactions if enabled
+if Config.UseTarget then
+    Citizen.CreateThread(function()
+        exports['rsg-target']:AddTargetModel({GetHashKey(Config.Props.Pickup), GetHashKey(Config.Props.Processing)}, {
             options = {
                 {
+                    type = "client",
                     event = "thelux_weedjob:interact",
-                    icon = "fas fa-seedling",
-                    label = "Interact with Plant",
-                    action = function(entity)
-                        TriggerEvent('thelux_weedjob:interactWithProp', entity)
-                    end,
+                    icon = "fas fa-cannabis",
+                    label = "Interact",
                 },
             },
-            job = {'all'}, -- Allow all jobs to interact, customize as needed
             distance = 2.5
         })
+    end)
+end
+
+-- Interaction handler
+RegisterNetEvent('thelux_weedjob:interact')
+AddEventHandler('thelux_weedjob:interact', function(data)
+    local playerPed = PlayerPedId()
+    local coords = GetEntityCoords(playerPed)
+    local action
+
+    if #(coords - currentLocation.pickup) < 3.0 then
+        action = 'pickup'
+    elseif #(coords - currentLocation.process) < 3.0 then
+        action = 'process'
+    elseif #(coords - currentLocation.sell) < 3.0 then
+        action = 'sell'
+    end
+
+    if action and not isOnCooldown(action) then
+        setCooldown(action)
+        TriggerServerEvent('thelux_weedjob:' .. action)
+    elseif isOnCooldown(action) then
+        RSGCore.Functions.Notify(Lang:t('error.cooldown'), 'error')
     end
 end)
 
-RegisterNetEvent('thelux_weedjob:interactWithProp')
-AddEventHandler('thelux_weedjob:interactWithProp', function(entity)
-    local entityModel = GetEntityModel(entity)
-    if isOnCooldown() then
-        TriggerEvent('notification', Locales['en']['cooldown_message'])
-        return
-    end
-    lastActionTime = GetGameTimer()
-
-    if entityModel == GetHashKey(Config.PropPickup) then
-        TriggerServerEvent('thelux_weedjob:pickup')
-    elseif entityModel == GetHashKey(Config.PropProcessing) then
-        TriggerServerEvent('thelux_weedjob:process')
-    else
-        TriggerServerEvent('thelux_weedjob:sell')
-    end
-end)
-
+-- Key-based interaction if target system is disabled
 Citizen.CreateThread(function()
     while true do
-        local playerPed = PlayerPedId()
-        local coords = GetEntityCoords(playerPed)
-        local nearby = false
+        if not Config.UseTarget then
+            local playerPed = PlayerPedId()
+            local coords = GetEntityCoords(playerPed)
+            local sleep = 1000
 
-        if not Config.UseTargetSystem then
-            -- Key-based interaction
-            if GetDistanceBetweenCoords(coords, currentLocation.pickup, true) < 2.0 then
-                nearby = true
-                DisplayHelpTextThisFrame(Locales['en']['pickup_prompt'])
-                if IsControlJustReleased(0, 0xCEFD9220) and not isOnCooldown() then -- E key
-                    lastActionTime = GetGameTimer()
-                    TriggerServerEvent('thelux_weedjob:pickup')
-                end
-            elseif GetDistanceBetweenCoords(coords, currentLocation.process, true) < 2.0 then
-                nearby = true
-                DisplayHelpTextThisFrame(Locales['en']['process_prompt'])
-                if IsControlJustReleased(0, 0xCEFD9220) and not isOnCooldown() then -- E key
-                    lastActionTime = GetGameTimer()
-                    TriggerServerEvent('thelux_weedjob:process')
-                end
-            elseif GetDistanceBetweenCoords(coords, currentLocation.sell, true) < 2.0 then
-                nearby = true
-                DisplayHelpTextThisFrame(Locales['en']['sell_prompt'])
-                if IsControlJustReleased(0, 0xCEFD9220) and not isOnCooldown() then -- E key
-                    lastActionTime = GetGameTimer()
-                    TriggerServerEvent('thelux_weedjob:sell')
+            for action, loc in pairs({pickup = currentLocation.pickup, process = currentLocation.process, sell = currentLocation.sell}) do
+                local dist = #(coords - loc)
+                if dist < 2.0 then
+                    sleep = 0
+                    RSGCore.Functions.DrawText3D(loc.x, loc.y, loc.z, Lang:t('info.' .. action .. '_prompt'))
+                    if IsControlJustReleased(0, 0xCEFD9220) and not isOnCooldown(action) then -- E key
+                        setCooldown(action)
+                        TriggerServerEvent('thelux_weedjob:' .. action)
+                    elseif isOnCooldown(action) then
+                        RSGCore.Functions.Notify(Lang:t('error.cooldown'), 'error')
+                    end
                 end
             end
-        end
 
-        if not nearby then
-            Citizen.Wait(500)
+            Citizen.Wait(sleep)
         else
-            Citizen.Wait(0)
+            Citizen.Wait(1000)
         end
     end
 end)
-
-function DisplayHelpTextThisFrame(text)
-    SetTextComponentFormat("STRING")
-    AddTextComponentString(text)
-    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
-end
