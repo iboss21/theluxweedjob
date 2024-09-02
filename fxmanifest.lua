bf9bb30c1154e88f6e180197848558e3d34cbdd2
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-09-02 12:09:52 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,33 +3,28 @@ game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'TheLux RDR Weed Job'
-version '1.1.0'
+version '1.2.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
     '@rsg-core/shared/locale.lua',
-    'locales/en.lua',
-    'config.lua',
+    'locales/*.lua',
+    'config.lua'
 }
 
 client_scripts {
-    'client/client.lua',
-    'client/npcs.lua',
-    'client/props.lua',
+    'client/*.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/server.lua',
+    'server/*.lua'
 }
 
 dependencies {
     'rsg-core',
     'rsg-target',
-    'ox_lib',
-    'vorp_core',
-    'qbr-core',
-    'syn_framework',
+    'ox_lib'
 }
 
 lua54 'yes'
