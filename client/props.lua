8a330d1106ea89fe6c1da26cd6d5d47dfede6ce9
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-09-02 12:11:27 -0500

    Update props.lua

--- client/props.lua
+++ client/props.lua
@@ -1,33 +1,39 @@
+local RSGCore = exports['rsg-core']:GetCoreObject()
 local spawnedProps = {}
 
 -- Function to create a prop
-function createProp(propName, coords)
-    local prop = CreateObject(GetHashKey(propName), coords.x, coords.y, coords.z, false, false, true)
-    SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
+local function createProp(propName, coords)
+    local model = GetHashKey(propName)
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(1)
+    end
+    
+    local prop = CreateObject(model, coords.x, coords.y, coords.z - 1.0, false, false, true)
+    SetEntityHeading(prop, coords.w)
     PlaceObjectOnGroundProperly(prop)
-    table.insert(spawnedProps, prop)
+    FreezeEntityPosition(prop, true)
+    SetModelAsNoLongerNeeded(model)
+    
     return prop
 end
 
--- Function to remove all spawned props
-function removeAllProps()
+-- Spawn necessary props
+Citizen.CreateThread(function()
+    for _, location in pairs(Config.Locations) do
+        spawnedProps[#spawnedProps + 1] = createProp(Config.Props.Pickup, location.pickup)
+        spawnedProps[#spawnedProps + 1] = createProp(Config.Props.Processing, location.process)
+    end
+end)
+
+-- Clean up props when resource stops
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
     for _, prop in pairs(spawnedProps) do
         if DoesEntityExist(prop) then
             DeleteObject(prop)
         end
-    end
-    spawnedProps = {}
-end
-
--- Spawn necessary props at the start
-Citizen.CreateThread(function()
-    createProp(Config.PropPickup, currentLocation.pickup)
-    createProp(Config.PropProcessing, currentLocation.process)
-end)
-
--- Clean up props when resource stops
-AddEventHandler('onResourceStop', function(resource)
-    if resource == GetCurrentResourceName() then
-        removeAllProps()
     end
 end)
