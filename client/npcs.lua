68a7634aa1a0eeee5e9f904ea7a18fb233d1ee7f
Master Mind <48193931+iboss21@users.noreply.github.com>
2024-09-02 12:11:05 -0500

    Update npcs.lua

--- client/npcs.lua
+++ client/npcs.lua
@@ -1,98 +1,45 @@
-local npcModels = {
-    taskGiver = "u_m_m_asbminer_01", -- NPC for giving the weed picking task
-    processor = "u_m_m_asbminer_01", -- NPC for processing the weed
-    buyer = "u_m_m_asbminer_01", -- NPC for selling the processed weed
-}
-
-local npcLocations = {
-    taskGiver = vector3(-1392.21, -2650.47, 42.92), -- Location of the task giver NPC
-    processor = vector3(-1387.56, -2660.53, 42.92), -- Location of the processing NPC
-    buyer = vector3(-1378.48, -2673.44, 42.92), -- Location of the buyer NPC
-}
-
+local RSGCore = exports['rsg-core']:GetCoreObject()
 local spawnedNpcs = {}
 
 -- Function to spawn an NPC
-function spawnNpc(model, coords, heading)
-    RequestModel(GetHashKey(model))
-    while not HasModelLoaded(GetHashKey(model)) do
+local function spawnNpc(npcConfig, coords)
+    local model = GetHashKey(npcConfig.model)
+    RequestModel(model)
+    while not HasModelLoaded(model) do
         Wait(1)
     end
-
-    local npc = CreatePed(3, GetHashKey(model), coords.x, coords.y, coords.z, heading or 0.0, false, true)
+    
+    local npc = CreatePed(model, coords.x, coords.y, coords.z - 1.0, coords.w, false, false, 0, 0)
     SetEntityAsMissionEntity(npc, true, true)
-    SetPedDiesWhenInjured(npc, false)
-    SetPedCanPlayAmbientAnims(npc, true)
-    SetPedCanRagdollFromPlayerImpact(npc, false)
-    TaskStartScenarioInPlace(npc, "WORLD_HUMAN_STAND_IMPATIENT", 0, true)
-    table.insert(spawnedNpcs, npc)
+    SetPedCanBeTargetted(npc, false)
+    SetBlockingOfNonTemporaryEvents(npc, true)
+    SetEntityInvincible(npc, true)
+    FreezeEntityPosition(npc, true)
+
+    if npcConfig.scenario then
+        TaskStartScenarioInPlace(npc, npcConfig.scenario, 0, true)
+    end
+
     return npc
 end
 
--- Spawn NPCs on resource start
+-- Spawn NPCs
 Citizen.CreateThread(function()
-    -- Spawn the task giver NPC
-    spawnNpc(npcModels.taskGiver, npcLocations.taskGiver)
-
-    -- Spawn the processing NPC
-    spawnNpc(npcModels.processor, npcLocations.processor)
-
-    -- Spawn the buyer NPC
-    spawnNpc(npcModels.buyer, npcLocations.buyer)
-end)
-
--- Function to handle NPC interactions
-function handleNpcInteraction(npcType)
-    if npcType == 'taskGiver' then
-        -- Trigger the event to start picking weed task
-        TriggerServerEvent('thelux_weedjob:startTask')
-    elseif npcType == 'processor' then
-        -- Trigger the event to process weed
-        TriggerServerEvent('thelux_weedjob:process')
-    elseif npcType == 'buyer' then
-        -- Trigger the event to sell processed weed
-        TriggerServerEvent('thelux_weedjob:sell')
-    end
-end
-
--- Detect player proximity to NPCs and show prompts
-Citizen.CreateThread(function()
-    while true do
-        local playerPed = PlayerPedId()
-        local coords = GetEntityCoords(playerPed)
-        local nearby = false
-
-        for npcType, location in pairs(npcLocations) do
-            if GetDistanceBetweenCoords(coords, location, true) < 2.0 then
-                nearby = true
-                DisplayHelpTextThisFrame("Press [E] to interact with the " .. npcType)
-                if IsControlJustReleased(0, 0xCEFD9220) then -- E key
-                    handleNpcInteraction(npcType)
-                end
-            end
-        end
-
-        if not nearby then
-            Citizen.Wait(500)
-        else
-            Citizen.Wait(0)
-        end
+    for _, location in pairs(Config.Locations) do
+        spawnedNpcs[#spawnedNpcs + 1] = spawnNpc(Config.NPCs.TaskGiver, location.pickup)
+        spawnedNpcs[#spawnedNpcs + 1] = spawnNpc(Config.NPCs.Processor, location.process)
+        spawnedNpcs[#spawnedNpcs + 1] = spawnNpc(Config.NPCs.Buyer, location.sell)
     end
 end)
 
 -- Clean up NPCs when resource stops
-AddEventHandler('onResourceStop', function(resource)
-    if resource == GetCurrentResourceName() then
-        for _, npc in ipairs(spawnedNpcs) do
-            if DoesEntityExist(npc) then
-                DeleteEntity(npc)
-            end
+AddEventHandler('onResourceStop', function(resourceName)
+    if (GetCurrentResourceName() ~= resourceName) then
+        return
+    end
+    for _, npc in pairs(spawnedNpcs) do
+        if DoesEntityExist(npc) then
+            DeleteEntity(npc)
         end
     end
 end)
-
-function DisplayHelpTextThisFrame(text)
-    SetTextComponentFormat("STRING")
-    AddTextComponentString(text)
-    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
-end
